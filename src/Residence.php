c31143f4be2e3e49eff6f43c7b15c8461bdf4653
اسماعیل زارع <zarehesmaiel@gmail.com>
2020-05-04 14:39:43 +0430

    New implementation

--- src/Residence.php
+++ src/Residence.php
@@ -3,25 +3,36 @@
 namespace Armincms\Irmoble;
 
 use Armincms\Koomeh\Nova\Residence as Resource; 
-use Illuminate\Http\Request; 
-use Armincms\Wizard\Step; 
+use Illuminate\Http\Request;  
+use Laravel\Nova\Http\Requests\NovaRequest;
 
 use Armincms\Tab\Tab; 
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\Heading;
 use Laravel\Nova\Fields\BelongsTo;
 use Armincms\Nova\Fields\Images;
-use Armincms\Fields\BelongsToMany;
-use Armincms\Fields\MorphedByMany;
-use Armincms\Facility\Facility as FacilityModel;
+use Armincms\Fields\BelongsToMany; 
+use Armincms\Koomeh\Nova\Facility;
 use OwenMelbz\RadioField\RadioButton;
-use Armincms\RawData\Common;
-use Armincms\Facility\Nova\Fields\ManyToMany;
+use Armincms\RawData\Common; 
 use Armincms\Koomeh\Nova\Configuration;
+use Zareismail\NovaWizard\Contracts\Wizard;
+use Zareismail\NovaWizard\Step;
+use Armincms\Koomeh\Nova\ResidencesType;
+use Armincms\Koomeh\Nova\ResidencesUsage;
+use Armincms\Koomeh\Nova\ResidencesPricing;
+use Armincms\Koomeh\Nova\ResidencesCondition;
+use Armincms\Koomeh\Nova\ResidencesReservation;
+use Armincms\Location\Location;
+use Armincms\Location\Nova\Zone;
+use Armincms\Location\Nova\City;
+use Hubertnnn\LaravelNova\Fields\DynamicSelect\DynamicSelect;
+use Manmohanjit\BelongsToDependency\BelongsToDependency;
 
-class Residence extends Resource
+class Residence extends Resource implements Wizard
 {     
     /**
      * Indicates if the resource should be displayed in the sidebar.
@@ -29,6 +40,11 @@ class Residence extends Resource
      * @var bool
      */
     public static $displayInNavigation = false;
+
+    public static $pricings;
+    public static $facilities;
+
+    public $width = ['pricings'];
 
     /**
      * Get the fields displayed by the resource.
@@ -39,104 +55,68 @@ class Residence extends Resource
     public function fields(Request $request)
     { 
         return [  
-            Step::make(__("Residences Specifications"), [$this, 'stepOne']),
+            (new Step(__("Residences Specifications"), $this->stepOne($request)))
+                ->checkpoint()
+                ->withToolbar(),
 
-            Step::make(__("Facilities"), [$this, 'StepTwo']),
+            new Step(__("Pricing"), $this->stepTwo($request)),  
 
-            // Step::make('Step One', [
-            //     Text::make(__('INN'), 'inn')->hideFromIndex(),
-            // ]),
-
-            // Step::make('Step Two', [
-            //     Text::make(__('INN'), 'inn')->readonly()->hideFromIndex(),
-            //     Text::make(__('Title'), function ()  {
-            //         return time();
-            //     })->readonly()->hideFromIndex(),
-
-            //     Text::make(__('email'), 'email')->rules('required')->hideFromIndex(),
-            // ]),
-
-            // Step::make(__("Conditions"), [$this, 'conditionFields']),
-                
-            // Step::make(__("Settings"), [$this, 'settingFields']),
+            $this->when($this->getFacilities()->count(), new Step(__("Facilities"), $this->stepThree($request))), 
         ];
     }
 
 
-    public function stepOne()
+    public function stepOne($request)
     {
+        $locations = Location::get(); 
+
         return  [
             ID::make()->sortable(),
 
-            BelongsTo::make(
-                __("Residences Type"),
-                'residencesType',
-                \Armincms\Koomeh\Nova\ResidencesType::class
-            )->withoutTrashed(),
+            BelongsTo::make(__("Residences Type"), 'residencesType', ResidencesType::class)
+                ->withoutTrashed(),
 
-            BelongsTo::make(
-                    __("Reservation Method"),
-                    'reservation',
-                    \Armincms\Koomeh\Nova\ResidencesReservation::class
-                )
+            BelongsTo::make(__("Reservation Method"), 'reservation', ResidencesReservation::class)
                 ->withoutTrashed()
                 ->hideFromIndex(! (bool) Configuration::option("_residences_reservation_")) 
                 ->hideFromDetail(! (bool) Configuration::option("_residences_reservation_")) 
                 ->hideWhenCreating(! (bool) Configuration::option("_residences_reservation_")) 
-                ->hideWhenUpdating(! (bool) Configuration::option("_residences_reservation_")), 
+                ->hideWhenUpdating(! (bool) Configuration::option("_residences_reservation_")),
 
-            BelongsTo::make(__("City"), 'city', \Armincms\Location\Nova\City::class)
-                ->searchable()
-                ->hideFromIndex(),
+            BelongsTo::make(__('City'), 'city', City::class)
+                ->hideFromIndex(), 
 
-            BelongsTo::make(__("Zone"), 'zone', \Armincms\Location\Nova\Zone::class)
-                ->searchable()
-                ->hideFromIndex(),
-
+            BelongsToDependency::make(__('Zone'), 'zone', Zone::class)
+                ->dependsOn('city', 'location_id')
+                ->hideFromIndex(), 
+ 
             Text::make(__("Location"), function() {
                 return optional($this->zone)->name ." - ".optional($this->city)->name;
-            }),
+            })->onlyOnIndex(),
 
-            BelongsToMany::make(
-                    __("Usage"), 'usages', \Armincms\Koomeh\Nova\ResidencesUsage::class
-                )
-                ->required()
-                ->rules('required')
-                ->display("usage"), 
+            $this->when($count = ResidencesUsage::newModel()->count() > 1, function() {
+                return  BelongsToMany::make(__("Usage"), 'usages', ResidencesUsage::class)
+                            ->required()
+                            ->rules('required')
+                            ->display("usage");
+            }),  
 
-            BelongsToMany::make(
-                    __("Pricings"), 'pricings', \Armincms\Koomeh\Nova\ResidencesPricing::class
-                )
-                ->fields(function ($request) {
-                    $pricing = \Armincms\Koomeh\Nova\ResidencesPricing::newModel()->find($request->relatedId);
+            $this->when($count == false && ! $request->isMethod('get'), function() {
+                if($usage = ResidencesUsage::newModel()->first()) {
+                    return  Text::make('usages')->fillUsing(function($request, $model) use ($usage) {
+                                $model::saved(function($model) use ($request, $usage) {
+                                    $model->usages()->sync($usage);
+                                });
+                            }); 
+                }
+            }),   
 
-                    return $pricing->adaptive ? [] : [
+            Text::make(__('Price'), function() {
+                return $this->pricings->map(function($pricing) {
+                    return "{$pricing->label}:".($pricing->pivot->adaptive ? __('Adaptive') : $pricing->pivot->price);
+                })->implode(' - ');
+            })->asHtml(),      
 
-                        RadioButton::make(__("Adaptive"), "adaptive")->options([
-                            __("No"), __("Yes")
-                        ])->toggle([
-                            1 => ['price']
-                        ])->required()->default(0),
-
-
-                        $this->priceField("Price", 'price', option("_residences_currency_", "IRR"))
-                            ->required() 
-                            ->rules(['numeric', function ($attribute, $value, $fail) {
-                                if (request('adaptive') == 0 && floatval($value) <= 0) {
-                                    $fail(__("You should enter valid price"));
-                                }
-                            }]),
-
-                    ];
-                })
-                ->pivots()
-                ->fillUsing(function ($value) {
-                    return [
-                        'adaptive' => (int) ($value['adaptive'] ?? 0),
-                        'price' => floatval($value['price'] ?? 0),
-                    ];
-                }),
- 
             $this
                 ->imageField() 
                 ->stacked()
@@ -144,82 +124,144 @@ class Residence extends Resource
                     $this->toggle(__("Master"), "master"),
                 ]),
 
-            $this->translatable([ 
-                $this->abstractField(),
-
-                $this->gutenbergField(),
-            ]),
+            $this->abstractField(_('Write about your home')),
         ];
     }
 
-    public function stepTwo()
+    public function stepTwo($request)
+    {  
+        return $this->getPricings()->map(function($pricing, $index) {
+            $pivot = optional($this->pricings)->count() 
+                        ? optional($this->pricings->where('id', $pricing->id)->first())->pivot
+                        : null; 
+
+            return [
+                Heading::make($pricing->label),
+
+                RadioButton::make(__("Adaptive"), "pricings[{$pricing->id}][adaptive]")
+                        ->options([
+                            1 => __("Yes"), 0  => __("No"), 
+                        ])
+                        ->toggle([
+                            1 => ["pricings[{$pricing->id}][price]"]
+                        ])
+                        ->required()
+                        ->default(0)
+                        ->fillUsing(function() {})
+                        ->resolveUsing(function($value, $resource, $attribute) use ($pivot) { 
+                            return optional($pivot)->adaptive;
+                        })
+                        ->onlyOnForms(),
+
+                $this->priceField("Price", "pricings[{$pricing->id}][price]", option("_residences_currency_", "IRR"))
+                        ->required() 
+                        ->rules([
+                            'numeric', 
+                            function ($attribute, $value, $fail) {
+                                if (request('adaptive') == 0 && floatval($value) <= 0) {
+                                    $fail(__("You should enter valid price"));
+                                }
+                            }
+                        ])
+                        ->fillUsing(function($request, $model) use ($index) {
+                            if($this->getPricings()->count() - $index == 1) {
+                                $model::saved(function($model) use ($request) {
+                                    $model->pricings()->sync((array) $request->get('pricings'));  
+                                }); 
+                            }
+                        })
+                        ->resolveUsing(function($value, $resource, $attribute) use ($pivot) { 
+                            return optional($pivot)->price;
+                        })
+                        ->onlyOnForms()
+            ];
+        })->flatten(1)->map->onlyOnForms()->toArray(); 
+    }
+
+    public function stepThree()
     {
-        return [
-            // MorphedByMany::make(
-            //         __("Facilities"), 'facilities', \Armincms\Koomeh\Nova\Facility::class
-            //     )
-            //     ->fields(function ($request) {
-            //         $fieldClass = data_get(
-            //             $facility = FacilityModel::find($request->relatedId),
-            //             'field'
-            //         );
+        return $this->getFacilities()->map(function($facility, $index) {
+            $fieldClass = $facility->field;
+            $attribute = "facilities[{$facility->id}][value]";
 
-            //         if (! class_exists($fieldClass)) {
-            //             $fieldClass = Text::class;
-            //         }
+            if (! class_exists($fieldClass)) {
+                $fieldClass = Text::class;
+            }
 
-            //         return [tap($fieldClass::make($facility->label, 'value'), function ($field) use ($facility) {
-            //             if (method_exists($field, 'options')) {
-            //                 $field
-            //                     ->options(collect($facility->options)->pluck('text'))
-            //                     ->required()
-            //                     ->rules('required');
-            //             }
+            $fillUsing = $this->getFacilities()->count() - $index !== 1 
+                            ? function() {} 
+                            : function($request, $model, $attribute) {
+                                $model::saved(function($model) use ($request) {
+                                    $model->facilities()->sync(
+                                        collect($request->get('facilities'))->filter(function($facility) {
+                                            return ! empty($facility['value']);
+                                        })->map(function($facility) {
+                                            if(is_array($facility['value'])) {
+                                                $facility['value'] = json_encode($facility['value']);
+                                            }
 
-            //             if (method_exists($field, 'displayUsingLabels')) {
-            //                 $field->displayUsingLabels();
-            //             }
-            //         })];
-            //     })
-            //     ->pivots()
-            //     ->hideFromIndex()
-            //     ->fillUsing(function ($value) {
-            //         if (isset($value['value']) && is_array($value['value'])) {
-            //             $value['value'] = json_encode($value['value']);
-            //         }
+                                            return $facility;
+                                        })->toArray()
+                                    );
+                                });
+                            };
 
-            //         return $value;
-            //     }), 
+            return tap($fieldClass::make($facility->label, $attribute)->fillUsing($fillUsing), function($field) use ($facility) {
+                    if (method_exists($field, 'options')) {
+                        $field->options(collect($facility->options)->pluck('text')) ;
+                    }
 
+                    if (method_exists($field, 'displayUsingLabels')) {
+                        $field->displayUsingLabels();
+                    }
 
-            ManyToMany::make("", "Numbers")->onlyNumbers(), 
+                    $field->hideFromIndex();
+                    $field->resolveUsing(function($value, $resource) use ($facility) {
+                        $facilities = optional($this->facilities)->where('id', $facility->id);
 
-            ManyToMany::make("", "Others")->sortUsing(function($field, $index) { 
-                return ! method_exists($field, 'options') ?: 0;
-            })->withoutNumbers(), 
+                        if(optional($facilities)->count()) {
+                            return $facilities->first()->pivot->value;
+                        }
+                    });
+            });
+        })->toArray(); 
+    }
 
-            BelongsToMany::make(
-                    __("Residences Condition"), 
-                    'conditions', 
-                    \Armincms\Koomeh\Nova\ResidencesCondition::class
-                )
-                ->hideFromIndex()
-                ->display("condition")
-                ->hideFromIndex(), 
+    public function getPricings()
+    {
+        if(! isset(static::$pricings)) {
+            static::$pricings = ResidencesPricing::newModel()->get(); 
+        } 
 
-            Text::make(__("Latitude"), 'latitude')
-                    ->hideFromIndex(),
+        return static::$pricings;
+    }
 
-            Text::make(__("Longitude"), 'longitude')
-                    ->hideFromIndex(),
+    public function getFacilities()
+    {
+        if(! isset(static::$facilities)) {
+            static::$facilities = Facility::newModel()->get(); 
+        } 
 
+        return static::$facilities->sortBy(function($facility) { 
+            switch (class_basename($facility->field)) {
+                case 'Number':
+                    return 0;
+                    break;
 
-            $this->translatable([
-                Text::make(__("Address"), 'address')
-                    ->nullable()
-                    ->hideFromIndex(), 
-            ]),
-        ];
+                case 'Text':
+                    return 1;
+                    break;
+
+                case 'Boolean':
+                    return time();
+                    break;
+                    break; 
+                
+                default:
+                    return 3;
+                    break;
+            }
+        });
     }
 
 
